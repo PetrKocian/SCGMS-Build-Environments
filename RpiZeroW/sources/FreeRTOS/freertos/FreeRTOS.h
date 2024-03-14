211b4820abb85e7fdefa4cf7811cd0f9fa3e6e2a
petrkocian25@gmail.com <petrkocian25@gmail.com>
2024-03-14 09:18:04 +0100

    added portTICK_PERIOD_MS define

--- RpiZeroW/sources/FreeRTOS/freertos/FreeRTOS.h
+++ RpiZeroW/sources/FreeRTOS/freertos/FreeRTOS.h
@@ -843,7 +843,8 @@ V8 if desired. */
 	#define xTimerHandle TimerHandle_t
 	#define xCoRoutineHandle CoRoutineHandle_t
 	#define pdTASK_HOOK_CODE TaskHookFunction_t
-	#define portTICK_RATE_MS portTICK_PERIOD_MS
+	#define portTICK_RATE_MS 1
+	#define portTICK_PERIOD_MS 1
 	#define pcTaskGetTaskName pcTaskGetName
 	#define pcTimerGetTimerName pcTimerGetName
 	#define pcQueueGetQueueName pcQueueGetName
